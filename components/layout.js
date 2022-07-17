8253af7752d25fa91cc172eaecd4dc59fda5bf3a
ChristopherChantres <chris.chantres@gmail.com>
2022-07-16 22:59:40 -0400

    Dynamic routes added. Data fetch to get the post's content

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Christopher';
+const name = 'Christopher Chantres';
 export const siteTitle = 'Christopher\'s Blog';
 
 export default function Layout({ children, home }) {
